adfc54cdc793c6408131ccdfa82af8f6373f64fa
Christine Tham <chris.tham@hellotham.com>
2024-12-12 20:19:35 +1100

    MVP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 import sitemap from "@astrojs/sitemap"
+import alpinejs from '@astrojs/alpinejs'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-base.netlify.app',
-  integrations: [
-    UnoCSS({
-      injectReset: true
-    }),
-    sitemap()
-  ]
+  site: 'https://astro-pines.netlify.app',
+  integrations: [UnoCSS({
+    injectReset: true
+  }), sitemap(), alpinejs()]
 });
